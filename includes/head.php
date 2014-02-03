d43806e169d58df7573f4e61b5de8f314edb3efd
ajenshireman <awshireman@pstcc.edu>
2014-02-03 10:51:14 -0500

    refactored all tabs to spaces

--- includes/head.php
+++ includes/head.php
@@ -1,8 +1,8 @@
-<?php 
+<?php
 /**
  * head.php
  * Author: Ajen Shireman
- * 
+ *
  * Common <head> for all pages
  */
 ?>
@@ -10,15 +10,15 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
 <head>
-	<meta charset="utf-8" />
-	
-	<title></title>
-	
-	<!-- normalize.css  -->
-	<link rel="stylesheet" href="css/normaize.css" />
-	
-	<!-- custom styles -->
-	<link re="stylesheet" href="css/styles.css" />
-
+    <meta charset="utf-8" />
+    
+    <title></title>
+    
+    <!-- normalize.css  -->
+    <link rel="stylesheet" href="css/normaize.css" />
+    
+    <!-- custom styles -->
+    <link re="stylesheet" href="css/styles.css" />
+    
 </head>
 <body>
