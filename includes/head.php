100679274773d4630520b042d7c29324dc898218
ajenshireman <awshireman@pstcc.edu>
2014-02-03 10:39:13 -0500

    head.php and foot.php

---
+++ includes/head.php
@@ -0,0 +1,24 @@
+<?php 
+/**
+ * head.php
+ * Author: Ajen Shireman
+ * 
+ * Common <head> for all pages
+ */
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+	
+	<title></title>
+	
+	<!-- normalize.css  -->
+	<link rel="stylesheet" href="css/normaize.css" />
+	
+	<!-- custom styles -->
+	<link re="stylesheet" href="css/styles.css" />
+
+</head>
+<body>
+	
