d6fc4b3ff292230d73ccfc7979fb8fdf3e635ee5
ajenshireman <awshireman@pstcc.edu>
2014-02-03 10:49:08 -0500

    added charset meta

--- includes/head.php
+++ includes/head.php
@@ -10,6 +10,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html lang="en">
 <head>
+	<meta charset="utf-8" />
 	
 	<title></title>
 	
@@ -21,4 +22,3 @@
 
 </head>
 <body>
-	
