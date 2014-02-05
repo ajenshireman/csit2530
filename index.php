a0895fae63a3b64f60ec2b387f32cb59553760d0
ajenshireman <awshireman@pstcc.edu>
2014-02-05 12:49:38 -0500

    Added head and foot incudes to index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
+<?php include './includes/head.php' ?>
 <ul>
     <li><a href="display_phpinfo.php">phpinfo()</a></li>
     <li><a href="lab2/orderform.html">Lab 2 Part A</a></li>
     <li><a href="lab2/pickList.php">Lab2 PartB</a></li>
 </ul>
+<?php include './includes/foot.php' ?>
