2d9d6858fadab22f60ebae8a03cc0382e703ce2a
ajenshireman <awshireman@pstcc.edu>
2014-02-10 11:03:54 -0500

    added lab3 links to the main index

--- index.php
+++ index.php
@@ -3,5 +3,11 @@
     <li><a href="display_phpinfo.php">phpinfo()</a></li>
     <li><a href="orderform.html">Lab 2 Part A</a></li>
     <li><a href="pickList.php">Lab2 PartB</a></li>
+    <li>Lab 3
+        <ul>
+            <li><a href="lab3/futurevalue">Future Value</a></li>
+            <li><a href="lab3/dicountcalc">Discount Calculator</a></li>
+        </ul>
+    </li>
 </ul>
 <?php include './includes/foot.php' ?>
