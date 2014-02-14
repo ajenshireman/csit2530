f939f233f4088ff22b61e130ce4f1c4582ffa524
Ajen Shireman <ajenshireman@gmail.com>
2014-02-14 13:53:17 -0500

    Moved class header to php block

--- lab3/discountcalc/index.php
+++ lab3/discountcalc/index.php
@@ -1,8 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<!-- 
+<?php 
 /**
  * index.php
  * Ajen Shireman
@@ -14,8 +10,11 @@
  * Enhancements:
  * 
  */
- -->
-
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
     <title>Product Discount Calculator</title>
     <link rel="stylesheet" type="text/css" href="main.css" />
 </head>
