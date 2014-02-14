107df1fa3a4b6b98f9cabac13cdc733b0c0d084c
Ajen Shireman <ajenshireman@gmail.com>
2014-02-14 15:35:09 -0500

    Altered head and foot includes for foundation

--- includes/foot.php
+++ includes/foot.php
@@ -11,8 +11,19 @@
 
 </footer>
 
-<!--  Java Script -->
-<script src=""></script>
+<!--  javaScript -->
+<script src="/js/vendor/jquery.js"></script>
+<script src="/js/vendor/fastclick.js"></script>
 
+<!-- Foundation -->
+<script src="/js/foundation.min.js"></script>
+
+<!-- Custom javaScript -->
+<script src="/js/script.js"></script>
+
+<!-- Initialize Foundation -->
+<script>
+    $(document).foundation();
+</script>
 </body>
 </html>
--- includes/head.php
+++ includes/head.php
@@ -8,9 +8,11 @@
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html lang="en">
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html class="no-js" lang="en">
 <head>
     <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     
     <title>
         <?php 
@@ -25,8 +27,14 @@
     <!-- normalize.css  -->
     <link rel="stylesheet" href="css/normalize.css" />
     
+    <!-- Foundation 5.1.1 -->
+    <link rel="stylesheet" href="css/foundation.css">
+    
     <!-- custom styles -->
     <link rel="stylesheet" href="css/styles.css" />
+    
+    <!--  Foundation 5.1.1 html5 shim -->
+    <script src="/js/vendor/modernizr.js"></script>
     
 </head>
 <body>
