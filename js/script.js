8516213bf1957e264b5a1013d5f0a70a0e5644cc
ajenshireman <ajenshireman@gmail.com>
2014-02-15 14:26:00 -0500

    fixed typo

--- js/script.js
+++ js/script.js
@@ -9,5 +9,5 @@ $(function(){
 
 function clearForm ( form ) {
 	form.find('input[type=text], input[type=password], input[type=number], select, textarea').val('');
-	ftForm.find('input[type=checkbox], input[type=radio]').removeAttr('checked').removeAttr('selected');
+	form.find('input[type=checkbox], input[type=radio]').removeAttr('checked').removeAttr('selected');
 }
