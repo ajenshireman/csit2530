d471e830b7746deb109ebf2f95836585ca1bd9e9
ajenshireman <ajenshireman@gmail.com>
2014-02-15 13:40:41 -0500

    Converted form to use foundation

--- coursepicker.php
+++ coursepicker.php
@@ -9,113 +9,177 @@
  */
 ?>
 <?php include './includes/head.php' ?>
-
-<!-- 
-<form role="form" id="classSelectionForm" method="post" action="pickProcessor.php">
-    <div class="form-group">
-        <label for="inputName">Name</label>
-        <select name="inputName">
-            <option value="David Brown">David Brown</option>
-            <option value="Sharon Burlingame">Sharon Burlingame</option>
-            <option value="Gitti Negahban">Gitti Negahban</option>
-            <option value="Andrey Puretskiy">Andrey Puretskiy</option>
-        </select>
-    </div>
-    <div class="form-group">
-        <label for="inputSemester">Semester</label>
-        <select name="inputSemester">
-            <option value="Spring 2014">Spring 2014</option>
-            <option value="Fall 2014">Fall 2014</option>
-        </select>
-    </div>
-    <div class="form-group">
-        <label for="inputNumClasses">Number of Courses</label>
-        <input type="radio" name="inputNumClasses" value ="2" /> 2
-        <input type="radio" name="inputNumClasses" value ="3" /> 3
-        <input type="radio" name="inputNumClasses" value ="4" /> 4
-    </div>
-    <div class="form-group">
-        <label for="inputNumHours">Number of Hours</label>
-        <input type="text" name="inputNumHours" size="5" />
-    </div>
-	<div class="form-group">
-    	Courses (Select 5)<br />
-    	<div class="form-group">
-    	   <label for="inpo]utCourses">Choice 1 </label>
-        	<select name="inputCourses[]">
-        	    <option value="CSIT 1510 - Introduction to Programming Using Java">CSIT 1510 - Introduction to Programming Using Java</option>
-                <option value="CSIT 1520 - Advanced Java Programming">CSIT 1520 - Advanced Java Programming</option>
-                <option value="CSIT 1810 - Introduction to Database Design">CSIT 1810 - Introduction to Database Design</option>
-                <option value="CSIT 2230 - Introduction to Internet Software Development">CSIT 2230 - Introduction to Internet Software Development</option>
-                <option value="CSIT 2530 - Web Database Application Development">CSIT 2530 - Web Database Application Development</option>
-                <option value="CSIT 2860 - Machine Organization">CSIT 2860 - Machine Organization</option>
-                <option value="CSIT 2280 - Introduction to Scripting Languages">CSIT 2290 - Introduction to Scripting Languages</option>
-                <option value="CSIT 2520 - SQL Applications with Oracle">CSIT 2520 - SQL Applications with Oracle</option>
-        	</select>
-    	</div>
-    	<div class="form-group">
-    	   <label for="inputCourses">Choice 2 </label>
-        	<select name="inputCourses[]">
-        	    <option value="CSIT 1510 - Introduction to Programming Using Java">CSIT 1510 - Introduction to Programming Using Java</option>
-                <option value="CSIT 1520 - Advanced Java Programming">CSIT 1520 - Advanced Java Programming</option>
-                <option value="CSIT 1810 - Introduction to Database Design">CSIT 1810 - Introduction to Database Design</option>
-                <option value="CSIT 2230 - Introduction to Internet Software Development">CSIT 2230 - Introduction to Internet Software Development</option>
-                <option value="CSIT 2530 - Web Database Application Development">CSIT 2530 - Web Database Application Development</option>
-                <option value="CSIT 2860 - Machine Organization">CSIT 2860 - Machine Organization</option>
-                <option value="CSIT 2280 - Introduction to Scripting Languages">CSIT 2290 - Introduction to Scripting Languages</option>
-                <option value="CSIT 2520 - SQL Applications with Oracle">CSIT 2520 - SQL Applications with Oracle</option>
-        	</select>
-    	</div>
-    	<div class="form-group">
-    	   <label for="inputCourses">Choice 3 </label>
-        	<select name="inputCourses[]">
-        	    <option value="CSIT 1510 - Introduction to Programming Using Java">CSIT 1510 - Introduction to Programming Using Java</option>
-                <option value="CSIT 1520 - Advanced Java Programming">CSIT 1520 - Advanced Java Programming</option>
-                <option value="CSIT 1810 - Introduction to Database Design">CSIT 1810 - Introduction to Database Design</option>
-                <option value="CSIT 2230 - Introduction to Internet Software Development">CSIT 2230 - Introduction to Internet Software Development</option>
-                <option value="CSIT 2530 - Web Database Application Development">CSIT 2530 - Web Database Application Development</option>
-                <option value="CSIT 2860 - Machine Organization">CSIT 2860 - Machine Organization</option>
-                <option value="CSIT 2280 - Introduction to Scripting Languages">CSIT 2290 - Introduction to Scripting Languages</option>
-                <option value="CSIT 2520 - SQL Applications with Oracle">CSIT 2520 - SQL Applications with Oracle</option>
-        	</select>
-    	</div>
-    	<div class="form-group">
-    	   <label for="inputCourses">Choice 4 </label>
-        	<select name="inputCourses[]">
-        	    <option value="CSIT 1510 - Introduction to Programming Using Java">CSIT 1510 - Introduction to Programming Using Java</option>
-                <option value="CSIT 1520 - Advanced Java Programming">CSIT 1520 - Advanced Java Programming</option>
-                <option value="CSIT 1810 - Introduction to Database Design">CSIT 1810 - Introduction to Database Design</option>
-                <option value="CSIT 2230 - Introduction to Internet Software Development">CSIT 2230 - Introduction to Internet Software Development</option>
-                <option value="CSIT 2530 - Web Database Application Development">CSIT 2530 - Web Database Application Development</option>
-                <option value="CSIT 2860 - Machine Organization">CSIT 2860 - Machine Organization</option>
-                <option value="CSIT 2280 - Introduction to Scripting Languages">CSIT 2290 - Introduction to Scripting Languages</option>
-                <option value="CSIT 2520 - SQL Applications with Oracle">CSIT 2520 - SQL Applications with Oracle</option>
-        	</select>
-    	</div>
-    	<div class="form-group">
-    	   <label for="inputCourse">Choice 5 </label>
-        	<select name="inputCourses[]">
-        	    <option value="CSIT 1510 - Introduction to Programming Using Java">CSIT 1510 - Introduction to Programming Using Java</option>
-                <option value="CSIT 1520 - Advanced Java Programming">CSIT 1520 - Advanced Java Programming</option>
-                <option value="CSIT 1810 - Introduction to Database Design">CSIT 1810 - Introduction to Database Design</option>
-                <option value="CSIT 2230 - Introduction to Internet Software Development">CSIT 2230 - Introduction to Internet Software Development</option>
-                <option value="CSIT 2530 - Web Database Application Development">CSIT 2530 - Web Database Application Development</option>
-                <option value="CSIT 2860 - Machine Organization">CSIT 2860 - Machine Organization</option>
-                <option value="CSIT 2280 - Introduction to Scripting Languages">CSIT 2290 - Introduction to Scripting Languages</option>
-                <option value="CSIT 2520 - SQL Applications with Oracle">CSIT 2520 - SQL Applications with Oracle</option>
-        	</select>
-    	</div>
+<form action="">
+	<div class="row">
+	    <div class="small-12 columns">
+	    	<fieldset>
+	    	    <legend>Instructor and Course Information</legend>
+	    		<div class="small-12 medium-6 large-3 columns">
+	    		    <label for="inputName">Name
+	    		        <select name="inputName">
+	    		            <option value="David Brown">David Brown</option>
+	    		            <option value="Sharon Burlingame">Sharon Burlingame</option>
+	    		            <option value="Gitti Negahban">Gitti Negahban</option>
+	    		            <option value="Andrey Puretskiy">Andrey Puretskiy</option>
+	    		        </select>
+	    		    </label>
+	    		</div>
+	    		<div class="small-12 medium-6 large-3 columns">
+	    		    <label for="inputSemester">Semester
+	    		        <select name="inputSemester">
+	    		            <option value="Summer 2014">Spring 2014</option>
+	    		            <option value="Fall 2014">Fall 2014</option>
+	    		            <option value="Spring 2015">Spring 2015</option>
+	    		        </select>
+	    		    </label>
+	    		</div>
+	    		<div class="small-12 medium-6 large-3 columns">
+	    		    <label for="inputNumClases">Number of Courses
+	    		        <select name="inputNumClasses">
+	    		            <option value ="2">2</option>
+	    		            <option value ="3">3</option>
+	    		            <option value ="4">4</option>
+	    		        </select>
+	    		    </label>
+	    		</div>
+	    		<div class="small-12 medium-6 large-3 columns">
+	    		    <label for="inputNumHours">Number of Hours
+	    		        <input type="text" name="inputNumHours" size="5" />
+	    		    </label>
+	    		</div>
+	    	</fieldset>
+	    </div>
 	</div>
-	<div class="form-group">
-    	<label for="inputTimeslots" class="block">Preferred Times (Select at least 1)</label>
-    	<span class="block"><input type="checkbox" name="inputTimeslots[]" value="8:30-11:30"> 8:30-11:30</span>
-    	<span class="block"><input type="checkbox" name="inputTimeslots[]" value="9:30-11:30"> 9:30-11:30</span>
-    	<span class="block"><input type="checkbox" name="inputTimeslots[]" value="1:00-4:00"> 1:00-4:00</span>
-    	<span class="block"><input type="checkbox" name="inputTimeslots[]" value="4:00-6:00"> 4:00-6:00</span>
-    	<span class="block"><input type="checkbox" name="inputTimeslots[]" value="6:15-9:15"> 6:15-9:15</span>
-    	<span class="block"><input type="checkbox" name="inputTimeslots[]" value="No Fridays"> No Fridays</span>
+	<div class="row">
+		<div class="small-12 columns">
+			<fieldset>
+			    <legend>Desired Courses</legend>
+			    
+	            <div class="row">
+	            	<div class="small-12 medium-3 columns inline">
+	            		<label for="inputCourses">Choice 1:</label>
+	            	</div>
+	            	<div class="small-12 medium-9 columns">
+	            		<select name="inputCourses[]">
+	            		    <option value="CSIT 1510 - Introduction to Programming Using Java">CSIT 1510 - Introduction to Programming Using Java</option>
+	            		    <option value="CSIT 1520 - Advanced Java Programming">CSIT 1520 - Advanced Java Programming</option>
+	            		    <option value="CSIT 1810 - Introduction to Database Design">CSIT 1810 - Introduction to Database Design</option>
+	            		    <option value="CSIT 2230 - Introduction to Internet Software Development">CSIT 2230 - Introduction to Internet Software Development</option>
+	            		    <option value="CSIT 2530 - Web Database Application Development">CSIT 2530 - Web Database Application Development</option>
+	            		    <option value="CSIT 2860 - Machine Organization">CSIT 2860 - Machine Organization</option>
+	            		    <option value="CSIT 2280 - Introduction to Scripting Languages">CSIT 2290 - Introduction to Scripting Languages</option>
+	            		    <option value="CSIT 2520 - SQL Applications with Oracle">CSIT 2520 - SQL Applications with Oracle</option>
+	            		</select>
+	            	</div>
+	            </div>
+	            
+	            <div class="row">
+	            	<div class="small-12 medium-3 columns inline">
+	            		<label for="inputCourses">Choice 2:</label>
+	            	</div>
+	            	<div class="small-12 medium-9 columns">
+	            		<select name="inputCourses[]">
+	            		    <option value="CSIT 1510 - Introduction to Programming Using Java">CSIT 1510 - Introduction to Programming Using Java</option>
+	            		    <option value="CSIT 1520 - Advanced Java Programming">CSIT 1520 - Advanced Java Programming</option>
+	            		    <option value="CSIT 1810 - Introduction to Database Design">CSIT 1810 - Introduction to Database Design</option>
+	            		    <option value="CSIT 2230 - Introduction to Internet Software Development">CSIT 2230 - Introduction to Internet Software Development</option>
+	            		    <option value="CSIT 2530 - Web Database Application Development">CSIT 2530 - Web Database Application Development</option>
+	            		    <option value="CSIT 2860 - Machine Organization">CSIT 2860 - Machine Organization</option>
+	            		    <option value="CSIT 2280 - Introduction to Scripting Languages">CSIT 2290 - Introduction to Scripting Languages</option>
+	            		    <option value="CSIT 2520 - SQL Applications with Oracle">CSIT 2520 - SQL Applications with Oracle</option>
+	            		</select>
+	            	</div>
+	            </div>
+	            
+	            <div class="row">
+	            	<div class="small-12 medium-3 columns inline">
+	            		<label for="inputCourses">Choice 3:</label>
+	            	</div>
+	            	<div class="small-12 medium-9 columns">
+	            		<select name="inputCourses[]">
+	            		    <option value="CSIT 1510 - Introduction to Programming Using Java">CSIT 1510 - Introduction to Programming Using Java</option>
+	            		    <option value="CSIT 1520 - Advanced Java Programming">CSIT 1520 - Advanced Java Programming</option>
+	            		    <option value="CSIT 1810 - Introduction to Database Design">CSIT 1810 - Introduction to Database Design</option>
+	            		    <option value="CSIT 2230 - Introduction to Internet Software Development">CSIT 2230 - Introduction to Internet Software Development</option>
+	            		    <option value="CSIT 2530 - Web Database Application Development">CSIT 2530 - Web Database Application Development</option>
+	            		    <option value="CSIT 2860 - Machine Organization">CSIT 2860 - Machine Organization</option>
+	            		    <option value="CSIT 2280 - Introduction to Scripting Languages">CSIT 2290 - Introduction to Scripting Languages</option>
+	            		    <option value="CSIT 2520 - SQL Applications with Oracle">CSIT 2520 - SQL Applications with Oracle</option>
+	            		</select>
+	            	</div>
+	            </div>
+	            
+	            <div class="row">
+	            	<div class="small-12 medium-3 columns inline">
+	            		<label for="inputCourses">Choice 4:</label>
+	            	</div>
+	            	<div class="small-12 medium-9 columns">
+	            		<select name="inputCourses[]">
+	            		    <option value="CSIT 1510 - Introduction to Programming Using Java">CSIT 1510 - Introduction to Programming Using Java</option>
+	            		    <option value="CSIT 1520 - Advanced Java Programming">CSIT 1520 - Advanced Java Programming</option>
+	            		    <option value="CSIT 1810 - Introduction to Database Design">CSIT 1810 - Introduction to Database Design</option>
+	            		    <option value="CSIT 2230 - Introduction to Internet Software Development">CSIT 2230 - Introduction to Internet Software Development</option>
+	            		    <option value="CSIT 2530 - Web Database Application Development">CSIT 2530 - Web Database Application Development</option>
+	            		    <option value="CSIT 2860 - Machine Organization">CSIT 2860 - Machine Organization</option>
+	            		    <option value="CSIT 2280 - Introduction to Scripting Languages">CSIT 2290 - Introduction to Scripting Languages</option>
+	            		    <option value="CSIT 2520 - SQL Applications with Oracle">CSIT 2520 - SQL Applications with Oracle</option>
+	            		</select>
+	            	</div>
+	            </div>
+	            
+	            <div class="row">
+	            	<div class="small-12 medium-3 columns inline">
+	            		<label for="inputCourses">Choice 5:</label>
+	            	</div>
+	            	<div class="small-12 medium-9 columns">
+	            		<select name="inputCourses[]">
+	            		    <option value="CSIT 1510 - Introduction to Programming Using Java">CSIT 1510 - Introduction to Programming Using Java</option>
+	            		    <option value="CSIT 1520 - Advanced Java Programming">CSIT 1520 - Advanced Java Programming</option>
+	            		    <option value="CSIT 1810 - Introduction to Database Design">CSIT 1810 - Introduction to Database Design</option>
+	            		    <option value="CSIT 2230 - Introduction to Internet Software Development">CSIT 2230 - Introduction to Internet Software Development</option>
+	            		    <option value="CSIT 2530 - Web Database Application Development">CSIT 2530 - Web Database Application Development</option>
+	            		    <option value="CSIT 2860 - Machine Organization">CSIT 2860 - Machine Organization</option>
+	            		    <option value="CSIT 2280 - Introduction to Scripting Languages">CSIT 2290 - Introduction to Scripting Languages</option>
+	            		    <option value="CSIT 2520 - SQL Applications with Oracle">CSIT 2520 - SQL Applications with Oracle</option>
+	            		</select>
+	            	</div>
+	            </div>
+	            
+			</fieldset>
+		</div>
 	</div>
-	<button type="submit" name="btnSubmit">Submit</button>
+	<div class="row">
+	    <div class="small-12 columns">
+	    	<fieldset>
+	    	    <legend>Preferred Times (Select at least 1)</legend>
+	    		<div class="small-12 columns">
+	    			<div class="small-6 medium-3 columns">
+	    				<input type="checkbox" name="inputTimeslots[]" value="8:30-11:30"><label for="">8:30-11:30</label>
+	    			</div>
+	    			<div class="small-6 medium-3 columns">
+	    				<input type="checkbox" name="inputTimeslots[]" value="9:30-11:30"><label for="">9:30-11:30</label>
+	    			</div>
+	    			<div class="small-6 medium-3 columns">
+	    				<input type="checkbox" name="inputTimeslots[]" value="1:00-4:00"><label for="">1:00-4:00</label>
+	    			</div>
+	    			<div class="small-6 medium-3 columns">
+	    				<input type="checkbox" name="inputTimeslots[]" value="4:00-6:00"><label for="">4:00-6:00</label>
+	    			</div>
+	    			<div class="small-6 medium-3 columns">
+	    				<input type="checkbox" name="inputTimeslots[]" value="6:15-9:15"><label for="">6:15-9:15</label>
+	    			</div>
+	    			<div class="small-6 medium-3 columns end">
+	    				<input type="checkbox" name="inputTimeslots[]" value="No Fridays"><label for="">No Fridays</label>
+	    			</div>
+	    		</div>
+	    	</fieldset>
+	    </div>
+	</div>
+	<div class="row">
+	    <div class="small-12 columns">
+	        <button class="tiny" type="submit" name="btnSubmit">Submit</button>
+	        <button class="tiny" type="submit" name="btnClear">Cancel</button>
+	    </div>
+	</div>
 </form>
--->
 <?php include './includes/foot.php' ?>
