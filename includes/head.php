93b17dfe9b76bd03aa88794144727ef33ec5495b
ajenshireman <awshireman@pstcc.edu>
2014-02-19 10:01:09 -0500

    fixed meta tag formatting

--- includes/head.php
+++ includes/head.php
@@ -12,7 +12,7 @@
 <html class="no-js" lang="en">
 <head>
     <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     
     <title>
         <?php 
