ac07ad6d8b748de384350b7c13ed29a48a1cb32e
ajenshireman <awshireman@pstcc.edu>
2014-02-19 13:29:52 -0500

    put divs in a panel

--- processpicks.php
+++ processpicks.php
@@ -66,49 +66,55 @@ if ( isset($errors) ) {
 ?>
 <?php require './includes/head.php' ?>
 <div class="row">
-	<div id="name" class="small-12 columns">
-	    Name: <?php echo $name ?>
-	</div>
-	<div id="semester" class="small-12 columns">
-	    Semester: <?php echo $semester ?>
-	</div>
-	<div id="numClasses" class="small-12 columns">
-	    Number of classes: <?php echo $numClasses ?>
-	</div>
-	<div id="numHours" class="small-12 columns">
-	    Number of hours: <?php echo $numHours ?>
-	</div>
-	<div id="courses" class="small-12 columns">
-	    Preferred Courses: <br />
-	    <?php 
-	    if ( is_array($courses) ) {
-	        $p = 1;
-	        foreach ( $courses as $course ) {
-	            echo "Choice $p: $course<br />";
-	            $p++;
-	        }
-	    } else {
-	        echo $courses;
-	    }
-	    ?>
-	</div>
-	<div id="timeslots" class="small-12 columns">
-	    Preferred Times: <br />
-	    <?php
-	    if ( is_array($timeslots) ) {
-	        foreach ( $timeslots as $timeslot ) {
-	            echo "$timeslot<br />";
-	        }
-	    } else {
-	        echo $timeslots;
-	    }
-	    ?>
-	</div>
-	<?php 
-	if ( !empty($notes) ) {?>
-	<div id="notes" class="small-12 columns">
-	    Notes: <br />
-	    <?php echo $notes ?>
+	<div class="small-12 columns">
+		<div class="panel callout">
+			<div class="row">
+				<div id="name" class="small-12 columns">
+				    Name: <?php echo $name ?>
+				</div>
+				<div id="semester" class="small-12 columns">
+				    Semester: <?php echo $semester ?>
+				</div>
+				<div id="numClasses" class="small-12 columns">
+				    Number of classes: <?php echo $numClasses ?>
+				</div>
+				<div id="numHours" class="small-12 columns">
+				    Number of hours: <?php echo $numHours ?>
+				</div>
+				<div id="courses" class="small-12 columns">
+				    Preferred Courses: <br />
+				    <?php 
+				    if ( is_array($courses) ) {
+				        $p = 1;
+				        foreach ( $courses as $course ) {
+				            echo "Choice $p: $course<br />";
+				            $p++;
+				        }
+				    } else {
+				        echo $courses;
+				    }
+				    ?>
+				</div>
+				<div id="timeslots" class="small-12 columns">
+				    Preferred Times: <br />
+				    <?php
+				    if ( is_array($timeslots) ) {
+				        foreach ( $timeslots as $timeslot ) {
+				            echo "$timeslot<br />";
+				        }
+				    } else {
+				        echo $timeslots;
+				    }
+				    ?>
+				</div>
+				<?php 
+				if ( !empty($notes) ) {?>
+				<div id="notes" class="small-12 columns">
+				    Notes: <br />
+				    <?php echo $notes ?>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
 <?php } ?>
