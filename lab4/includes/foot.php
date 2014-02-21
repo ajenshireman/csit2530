73cf9bf57e49cd057129f6fd3b7f071be529a459
ajenshireman <awshireman@pstcc.edu>
2014-02-21 15:27:58 -0500

    added copyright and address info

--- lab4/includes/foot.php
+++ lab4/includes/foot.php
@@ -10,7 +10,16 @@
 </div> <!-- /wrapper -->
 
 <footer id="footer">
-    
+    <div class="row">
+        <div class="small-12 columns copyright">
+                &copy 2014 Pelissippi State Community College - Ajen Shireman
+        </div>
+    </div>
+    <div class="row">
+        <div class="small-12 coumns address">
+                10915 Hardin Valley Rd * P.O. Box 22990
+        </div>
+    </div>
 </footer>
 
 <!--  javaScript -->
