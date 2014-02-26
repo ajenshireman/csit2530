ca6d2d65a1f59f61e12605409eaefb51b8143e1d
ajenshireman <awshireman@pstcc.edu>
2014-02-26 12:31:06 -0500

    Copied files to new working dir

---
+++ lab5/includes/foot.php
@@ -0,0 +1,29 @@
+<?php 
+/**
+ * foot.php
+ * Author: Ajen Shireman
+ * 
+ * common EOF for all pages
+ */
+?>
+
+<footer>
+
+</footer>
+
+<!--  javaScript -->
+<script src="js/vendor/jquery.js"></script>
+<script src="js/vendor/fastclick.js"></script>
+
+<!-- Foundation -->
+<script src="js/foundation.min.js"></script>
+
+<!-- Custom javaScript -->
+<script src="js/script.js"></script>
+
+<!-- Initialize Foundation -->
+<script>
+    $(document).foundation();
+</script>
+</body>
+</html>
