53d6f148faa5e791c7a49075bde4dfeee138a0ea
ajenshireman <awshireman@pstcc.edu>
2014-02-26 14:00:13 -0500

    Added courses_db

---
+++ lab5/model/courses_db.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * courses_db.php
+ * Ajen Shireman
+ * CSIT 2530
+ * Course Selection app
+ * 
+ * Defines functions for accessing course information
+ * 
+ */
+
+/**
+ * Get the courses availble
+ * 
+ * For now pulls the information from a csv.
+ * 
+ * @todo 
+ * eventually pull from a database and return an associative array with field names as the keys
+ * 
+ * @return array 
+ */
+function getCourses () {
+    
+}
+
+?>
