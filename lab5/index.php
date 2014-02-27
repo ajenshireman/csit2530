c1674cfc9d3d3ad34a342aa3b05bfea8332e4c8e
Ajen Shireman <ajenshireman@gmail.com>
2014-02-27 01:39:15 -0500

    validation done via function

--- lab5/index.php
+++ lab5/index.php
@@ -3,9 +3,14 @@
  * index.php
  * Ajen Shireman
  * CSIT 2530 - Lab5
+ * 26 February 2014
  * 
  * Controller for course picker app
  */
+
+/* turn error reporting on */
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
 require './model/courses_db.php';  
 require './model/validation.php';     
@@ -24,7 +29,12 @@ if ( $action == 'display_selection_form' ) {
     // Show the form to the user
     require './coursepicker.php';
 } else if ( $action = 'courseSelectionSubmit' ) {
-    require './processpicks.php';
+    $formVars = validateCourseSelectionForm();
+    if ( isset($formVars['errors']) ) {
+        require './coursepicker.php';
+    } else {
+        require './processpicks.php';
+    }
 }
 
 ?>
