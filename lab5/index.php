fe04c0b8c2567d0a9ae23da6d2d0061644e2593f
Ajen Shireman <ajenshireman@gmail.com>
2014-02-27 00:06:58 -0500

    I need to commit more often so i remember what i did

--- lab5/index.php
+++ lab5/index.php
@@ -1,7 +1,29 @@
 <?php
 /**
+ * index.php
+ * Ajen Shireman
+ * CSIT 2530 - Lab5
+ * 
  * Controller for course picker app
  */
 
+require './model/courses_db.php';       
+
+if ( isset($_POST['action']) ) {
+    $action = $_POST['action'];
+} else if ( isset($_GET['action']) ) {
+    $action = $_GET['action'];
+} else {
+    $action = 'display_selection_form';
+}
+
+if ( $action == 'display_selection_form' ) {
+    unset($_POST);
+    
+    // Show the form to the user
+    include './coursepicker.php';
+} else if ( $action = 'courseSelectionSubmit' ) {
+    include './processpicks.php';
+}
 
 ?>
