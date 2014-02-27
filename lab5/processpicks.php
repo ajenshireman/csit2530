d83d76b35d6fde47bea21a5ca15c86802cb0d6d5
Ajen Shireman <ajenshireman@gmail.com>
2014-02-27 01:38:10 -0500

    variables pulled from $fromVars

--- lab5/processpicks.php
+++ lab5/processpicks.php
@@ -2,13 +2,16 @@
 /**
  * processpicks.php
  * Ajen Shireman
- * CSIT 2530 Lab4
- * 21 February 2014
+ * CSIT 2530 Lab5
+ * 216February 2014
  * 
  * This just echos back the choices from pickList.php
  * 
  * 21 February 2014
  *  Added pageTitle
+ *  
+ * 26 February 2014:
+ *  moved validation to function
  */
 
 $pageTitle = 'CSIT Faculty Course Selection - Results';
@@ -20,20 +23,21 @@ $pageTitle = 'CSIT Faculty Course Selection - Results';
 		<div class="panel callout">
 			<div class="row">
 				<div id="name" class="small-12 columns">
-				    Name: <?php echo $name ?>
+				    Name: <?php echo $formVars['values']['name'] ?>
 				</div>
 				<div id="semester" class="small-12 columns">
-				    Semester: <?php echo $semester ?>
+				    Semester: <?php echo $formVars['values']['semester'] ?>
 				</div>
 				<div id="numClasses" class="small-12 columns">
-				    Number of classes: <?php echo $numClasses ?>
+				    Number of classes: <?php echo $formVars['values']['numClasses'] ?>
 				</div>
 				<div id="numHours" class="small-12 columns">
-				    Number of hours: <?php echo $numHours ?>
+				    Number of hours: <?php echo $formVars['values']['numHours'] ?>
 				</div>
 				<div id="courses" class="small-12 columns">
 				    Preferred Courses: <br />
 				    <?php 
+				    $courses = $formVars['values']['courses'];
 				    if ( is_array($courses) ) {
 				        $p = 1;
 				        foreach ( $courses as $course ) {
@@ -48,6 +52,7 @@ $pageTitle = 'CSIT Faculty Course Selection - Results';
 				<div id="timeslots" class="small-12 columns">
 				    Preferred Times: <br />
 				    <?php
+				    $timeslots = $formVars['values']['timeslots'];
 				    if ( is_array($timeslots) ) {
 				        foreach ( $timeslots as $timeslot ) {
 				            echo "$timeslot<br />";
@@ -57,13 +62,10 @@ $pageTitle = 'CSIT Faculty Course Selection - Results';
 				    }
 				    ?>
 				</div>
-				<?php 
-				if ( !empty($notes) ) {?>
 				<div id="notes" class="small-12 columns">
 				    Notes: <br />
-				    <?php echo $notes ?>
+				    <?php echo $formVars['values']['notes'] ?>
 				</div>
-				<?php } ?>
 			</div>
 		</div>
 	</div>
