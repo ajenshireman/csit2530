dbfea09ae4ab6163f8f342157dcc0a497817f5af
Ajen Shireman <ajenshireman@gmail.com>
2014-02-27 01:41:14 -0500

    removed courseOptions.php

--- lab5/view/courseOptions.php
+++
@@ -1,8 +0,0 @@
-	            		    <option value="CSIT 1510 - Introduction to Programming Using Java">CSIT 1510 - Introduction to Programming Using Java</option>
-	            		    <option value="CSIT 1520 - Advanced Java Programming">CSIT 1520 - Advanced Java Programming</option>
-	            		    <option value="CSIT 1810 - Introduction to Database Design">CSIT 1810 - Introduction to Database Design</option>
-	            		    <option value="CSIT 2230 - Introduction to Internet Software Development">CSIT 2230 - Introduction to Internet Software Development</option>
-	            		    <option value="CSIT 2530 - Web Database Application Development">CSIT 2530 - Web Database Application Development</option>
-	            		    <option value="CSIT 2860 - Machine Organization">CSIT 2860 - Machine Organization</option>
-	            		    <option value="CSIT 2280 - Introduction to Scripting Languages">CSIT 2290 - Introduction to Scripting Languages</option>
-	            		    <option value="CSIT 2520 - SQL Applications with Oracle">CSIT 2520 - SQL Applications with Oracle</option>
