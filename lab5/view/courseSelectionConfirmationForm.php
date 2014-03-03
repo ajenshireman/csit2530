a4db67e3c05c37439c86acabe1d0d13481e29604
Ajen Shireman <ajenshireman@gmail.com>
2014-03-03 18:56:32 -0500

    put the display in a form and added confirm/back/cance buttons

--- lab5/view/courseSelectionConfirmationForm.php
+++ lab5/view/courseSelectionConfirmationForm.php
@@ -18,57 +18,66 @@ $pageTitle = 'CSIT Faculty Course Selection - Results';
 
 ?>
 <?php require './view/head.php' ?>
-<div class="row">
-	<div class="small-12 columns">
-		<div class="panel callout">
-			<div class="row">
-				<div id="name" class="small-12 columns">
-				    Name: <?php echo $formVars['values']['name'] ?>
-				</div>
-				<div id="semester" class="small-12 columns">
-				    Semester: <?php echo $formVars['values']['semester'] ?>
-				</div>
-				<div id="numClasses" class="small-12 columns">
-				    Number of classes: <?php echo $formVars['values']['numClasses'] ?>
-				</div>
-				<div id="numHours" class="small-12 columns">
-				    Number of hours: <?php echo $formVars['values']['numHours'] ?>
-				</div>
-				<div id="courses" class="small-12 columns">
-				    Preferred Courses: <br />
-				    <?php 
-				    $courses = $formVars['values']['courses'];
-				    if ( is_array($courses) ) {
-				        $p = 1;
-				        foreach ( $courses as $course ) {
-				            echo "Choice $p: $course<br />\n";
-				            $p++;
-				        }
-				    } else {
-				        echo $courses;
-				    }
-				    ?>
-				</div>
-				<div id="timeslots" class="small-12 columns">
-				    Preferred Times: <br />
-				    <?php
-				    $timeslots = $formVars['values']['timeslots'];
-				    if ( is_array($timeslots) ) {
-				        foreach ( $timeslots as $timeslot ) {
-				            echo "$timeslot<br />";
-				        }
-				    } else {
-				        echo $timeslots;
-				    }
-				    ?>
-				</div>
-				<div id="notes" class="small-12 columns">
-				    Notes: <br />
-				    <?php echo $formVars['values']['notes'] ?>
+<form id="courseSelectionConfirmationForm" action="." method="post">
+	<div class="row">
+		<div class="small-12 columns">
+			<div class="panel callout">
+				<div class="row">
+					<div id="name" class="small-12 columns">
+					    Name: <?php echo $formVars['values']['name'] ?>
+					</div>
+					<div id="semester" class="small-12 columns">
+					    Semester: <?php echo $formVars['values']['semester'] ?>
+					</div>
+					<div id="numClasses" class="small-12 columns">
+					    Number of classes: <?php echo $formVars['values']['numClasses'] ?>
+					</div>
+					<div id="numHours" class="small-12 columns">
+					    Number of hours: <?php echo $formVars['values']['numHours'] ?>
+					</div>
+					<div id="courses" class="small-12 columns">
+					    Preferred Courses: <br />
+					    <?php 
+					    $courses = $formVars['values']['courses'];
+					    if ( is_array($courses) ) {
+					        $p = 1;
+					        foreach ( $courses as $course ) {
+					            echo "Choice $p: $course<br />\n";
+					            $p++;
+					        }
+					    } else {
+					        echo $courses;
+					    }
+					    ?>
+					</div>
+					<div id="timeslots" class="small-12 columns">
+					    Preferred Times: <br />
+					    <?php
+					    $timeslots = $formVars['values']['timeslots'];
+					    if ( is_array($timeslots) ) {
+					        foreach ( $timeslots as $timeslot ) {
+					            echo "$timeslot<br />";
+					        }
+					    } else {
+					        echo $timeslots;
+					    }
+					    ?>
+					</div>
+					<div id="notes" class="small-12 columns">
+					    Notes: <br />
+					    <?php echo $formVars['values']['notes'] ?>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
+	<div class="row">
+        <div class="small-12 columns">
+            <button class="tiny" type="submit" name="action" value="courseSelectionConfirm">OK</button>
+            <button class="tiny" type="submit" name="action" value="courseSelectionRevist">Back</button>
+            <button class="tiny" type="cancel" name="action" value="courseSelectionCancel">Cancel</button>
+	    </div>
+	</div>
+</form>
 
 <?php require './view/foot.php' ?>
