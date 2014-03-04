89dbc0d40785d2a2cd0ff94e239b79e32d365165
Ajen Shireman <ajenshireman@gmail.com>
2014-03-03 19:55:39 -0500

    Added thank you page

---
+++ lab5/view/courseSelectionThankYou.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * courseSelectionThankYou.php
+ * Ajen Shireman
+ * CSIT 2530 - Lab5
+ * 03 March 2014
+ *
+ * Thank you page displayed after course selection is complete
+ */
+$pageTitle = 'CSIT Faculty Course Selection - Complete';
+?>
+<?php require './view/head.php' ?>
+<h1>DONE</h1>
+<?php require './view/foot.php' ?>
