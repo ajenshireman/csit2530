8079da28bf3dedc4f89e14722c25038e36f8b9d8
Ajen Shireman <ajenshireman@gmail.com>
2014-03-25 16:34:09 -0400

    common head and foot to separate files

---
+++ midterm/foot.php
@@ -0,0 +1,4 @@
+  <h4>by - Substitute Your Name and the date Here</h4>
+
+</body>
+</html>
---
+++ midterm/head.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml-transitional.dtd">
+<html>
+<head>
+  <title>CSIT 2530 BMI Calculator</title>
+  <meta content="text/html; charset=iso-8859-1" http-equiv=Content-Type>
+</head>
