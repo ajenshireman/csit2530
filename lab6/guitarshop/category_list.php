f18839926330d2debd01b68dbcf10088265cedd0
Ajen Shireman <ajenshireman@gmail.com>
2014-03-26 14:56:11 -0400

    added head and foot to category_list.php

--- lab6/guitarshop/category_list.php
+++ lab6/guitarshop/category_list.php
@@ -6,16 +6,7 @@
               ORDER BY categoryID';
     $categories = $db->query($query);
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-<!-- the head section -->
-<head>
-    <title>My Guitar Shop</title>
-    <link rel="stylesheet" type="text/css" href="main.css" />
-</head>
-
+<?php require 'head.php' ?>
 <!-- the body section -->
 <body>
     <div id="page">
@@ -47,12 +38,4 @@
 
     </div> <!-- end main -->
 
-    <div id="footer">
-        <p>
-            &copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.
-        </p>
-    </div>
-
-    </div><!-- end page -->
-</body>
-</html>
+<?php require 'foot.php' ?>
