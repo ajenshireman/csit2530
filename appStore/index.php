5d6a29f6401b09fe0167fbdf0a571b0de65478a4
Ajen Shireman <ajenshireman@gmail.com>
2014-03-31 10:32:36 -0400

    added .htaccess

--- appStore/index.php
+++ appStore/index.php
@@ -1,1 +1,14 @@
 <?php
+
+/* Error reporting */
+error_reporting(E_ALL);
+
+/* Set the timezone */
+date_default_timezone_set('America/New_York');
+
+/* Define Site path */
+$sitePath = realpath(dirname(__FILE__));
+define('__SITE_PATH', $sitePath);
+
+/* Initialize php variables */
+require 'includes/ini.php';
