bb3b337aeb15348556f4b39a72315c0460f94bff
Ajen Shireman <ajenshireman@gmail.com>
2014-04-07 18:47:00 -0400

    BURN IT DOWN

--- appStore/config/initialize.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* include base application classes */
-require SITE_PATH .'/application/' . 'application.class.php';
-require SITE_PATH .'/application/' . 'controller.class.php';
-require SITE_PATH .'/application/' . 'registry.class.php';
-require SITE_PATH .'/application/' . 'router.class.php';
-require SITE_PATH .'/application/' . 'template.class.php';
