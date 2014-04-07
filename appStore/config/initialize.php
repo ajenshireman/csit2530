a704655f84547693aba4c41bad10d90d0276e7aa
Ajen Shireman <ajenshireman@gmail.com>
2014-04-07 10:53:52 -0400

    load base application classes

--- appStore/config/initialize.php
+++ appStore/config/initialize.php
@@ -1,1 +1,6 @@
 <?php
+/* include base application classes */
+require SITE_PATH .'/aplication/' . 'controller.class.php';
+require SITE_PATH .'/aplication/' . 'reistry.class.php';
+require SITE_PATH .'/aplication/' . 'router.class.php';
+require SITE_PATH .'/aplication/' . 'template.class.php';
