29a48b77e3e86c128bc3c3e04fc6c4536efb0817
Ajen Shireman <ajenshireman@gmail.com>
2014-04-16 13:02:22 -0400

    use config.php contstants for paths

--- appstore/application/controller/main.php
+++ appstore/application/controller/main.php
@@ -1,26 +1,20 @@
 <?php
 class Main extends Controller {
     public function index () {
-        require './application/view/_template/head.php';
-        require './application/view/main/index.php';
-        require './application/view/_template/foot.php';
+        $this->render('main/index');
     }
     
     public function testLink () {
-        require './application/view/_template/head.php';
-        require './application/view/main/testlink.php';
-        require './application/view/_template/foot.php';
+        $this->render('main/testlink');
     }
     
     public function testParams ( $parameters = array() ) {
         if ( isset($parameters[0]) ) {
-            $msg = $parameters[0];
+            $this->msg = $parameters[0];
         } else {
-            $msg = 'There is no message';
+            $this->msg = 'There is no message';
         }
         
-        require './application/view/_template/head.php';
-        require './application/view/main/testparams.php';
-        require './application/view/_template/foot.php';
+        $this->render('main/testparams');
     }
 }
