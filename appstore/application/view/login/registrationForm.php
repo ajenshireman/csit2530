cc2ad7732c7c368bee54757b214519e69b5970f5
Ajen Shireman <ajenshireman@gmail.com>
2014-04-18 11:26:42 -0400

    registration form

---
+++ appstore/application/view/login/registrationForm.php
@@ -0,0 +1,22 @@
+<form name="registratioForm" action="<?php echo URL ?>/login/register" method="post">
+    <div>
+        <label for="registerUsername">Username: </label>
+        <input type="text" name="registerUsername />
+    </div>
+    <div>
+        <label for="registerEmail">Email: </label>
+        <input type="text" name="registerEmail />
+    </div>
+    <div>
+        <label for="registerPassword">Password: </label>
+        <input type="text" name="registerPassword />
+    </div>
+    <div>
+        <label for="registerPasswordConfirm">Confirm Password: </label>
+        <input type="text" name="registerPasswordConfirm />
+    </div>
+    <div>
+    	<button type="submit" id="btnLoginSubmit">Register</button>
+    	<button type="cancel" id="btnLoginCancel">Cancel</button>
+    </div>
+</form>
