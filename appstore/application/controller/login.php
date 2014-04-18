970a0f1ec1d900adf951cb90532e3db633d823d6
Ajen Shireman <ajenshireman@gmail.com>
2014-04-18 11:11:22 -0400

    added __construct(0 and renamned doLogin to login

--- appstore/application/controller/login.php
+++ appstore/application/controller/login.php
@@ -3,11 +3,21 @@
  * Handles user login and registration
  */
 class Login extends Controller {
+    public function __construct() {
+        parent::__construct();
+    }
+    
+    /**
+     * Show the login form
+     */
     public function index () {
         $this->render('login/loginForm');
     }
     
-    public function doLogin() {
+    /**
+     * proceses a login attemp login through the login form
+     */
+    public function login() {
         // make sure the user has entered a username and password
         if ( !isset($_POST['inputUsername']) || empty($_POST['inputUsername']) ) {
             return;
@@ -21,7 +31,9 @@ class Login extends Controller {
         $model = $this->loadModel('Login');
         if ( $model->login($tryUser, $tryPassword) ) {
             echo 'Login Success!';
+            // redirect to main page? or previous page
         } else {
+            // show the login form again
             echo 'Login Failed';
         }
     }
