2f577d4917338ed207fe2f9676eceff43c205fda
Ajen Shireman <ajenshireman@gmail.com>
2014-04-18 13:31:24 -0400

    verifyUserPassword()

--- appstore/application/model/loginmodel.php
+++ appstore/application/model/loginmodel.php
@@ -31,26 +31,70 @@ class LoginModel extends Model {
     }
     
     /**
+     * Gets the id for the user with the specified username
+     * 
+     * @param string $username
+     * 
+     * @return int
+     * 
+     * @throws Exception user not found
+     */
+    public function getUserId ( $username ) {
+        $stmnt = $this->db->prepare("select user.userId
+                                         from user
+                                         where user.username = :username
+                                            or user.email = :username");
+        $stmnt->execute(array(':username' => $username));
+        if ( $stmnt->rowCount() == 1  ) {
+            return $stmnt->fetchColumn(0);
+        } else {
+            throw new Exception($username . 'not found');
+        }
+    }
+    
+    /**
+     * Checks if the given passord matches the given users password
+     * 
+     * @param string $tryPassword
+     * @param int $userId
+     * 
+     * @return boolen
+     * 
+     * @throws Exception not exactly 1 result
+     */
+    public function verifyUserPassword ( $tryPassword, $userId ) {
+        $stmnt = $this->db->prepare("select password from user where userId = :userId");
+        $stmnt->execute(array(':userId' => $userId));
+        if ( $stmnt->rowCount() == 1  ) {
+            $password = $stmnt->fetchColumn(0);
+            if ( $tryPassword == $password ) { return true; } 
+            else { return false; }
+        } else {
+            throw new Exception('User not found');
+        }
+    }
+    
+    /**
      * Login process
      * 
      * @return boolean true is the login process was successful
      */
     public function login () {
         
-        if ( $this->usernameExists($_POST['loginUsername']) ) {
-            $stmnt = $this->db->prepare("select password from user where username = :username");
-            $stmnt->execute(array(':username' => $_POST['loginUsername']));
-            $password = $stmnt->fetchColumn(0);
-            
-            if ( $_POST['loginPassword'] == $password ) {
-                
-                return true;
-            } else {
+        try {
+            $userId = $this->getUserId($_POST['loginUsername']);
+            try {
+                if ( $this->verifyUserPassword($_POST['loginPassword'], $userId) ) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch ( Exception $e ) {
                 return false;
             }
-            //password_verify($_POST['inputPssword'], $)
-        } else {
+        } catch ( Exception $e) {
             // user not found
+            return false;
         }
         
     }
