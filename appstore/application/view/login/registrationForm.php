715896cac0d970fc032b55b7d65659cad82d9daf
Ajen Shireman <ajenshireman@gmail.com>
2014-04-18 11:31:30 -0400

    fixed label quotes

--- appstore/application/view/login/registrationForm.php
+++ appstore/application/view/login/registrationForm.php
@@ -1,19 +1,19 @@
 <form name="registratioForm" action="<?php echo URL ?>/login/register" method="post">
     <div>
         <label for="registerUsername">Username: </label>
-        <input type="text" name="registerUsername />
+        <input type="text" name="registerUsername" />
     </div>
     <div>
         <label for="registerEmail">Email: </label>
-        <input type="text" name="registerEmail />
+        <input type="text" name="registerEmail" />
     </div>
     <div>
         <label for="registerPassword">Password: </label>
-        <input type="text" name="registerPassword />
+        <input type="text" name="registerPassword" />
     </div>
     <div>
         <label for="registerPasswordConfirm">Confirm Password: </label>
-        <input type="text" name="registerPasswordConfirm />
+        <input type="text" name="registerPasswordConfirm" />
     </div>
     <div>
     	<button type="submit" id="btnLoginSubmit">Register</button>
