191a17e615184fbce30626c7825049c4bc1ecaf3
Ajen Shireman <ajenshireman@gmail.com>
2014-04-30 15:55:42 -0400

    form to change password

---
+++ appstore/application/view/account/passwordChangeForm.php
@@ -0,0 +1,23 @@
+<form name="changePasswordForm" action="<?php echoURL ?>account/changepassword" method="post">
+    <?php if ( isset($this->errors) ) { ?>
+    <div>
+        <?php echo $this->errors ?>
+    </div>
+    <?php } ?>
+    <div>
+        <label for="currentPassword">Current Password:</label>
+        <input type="text" name="currentPassword" />
+    </div>
+     <div>
+        <label for="newPassword">New Password:</label>
+        <input type="text" name="newPassword" />
+    </div>
+     <div>
+        <label for="confirmPassword">Confirm Password:</label>
+        <input type="text" name="confirmPassword" />
+    </div>
+    <div>
+    	<button type="submit" id="btnLoginSubmit">Submit</button>
+    	<button type="cancel" id="btnLoginCancel">Cancel</button>
+    </div>
+</form>
