ff2fcad7d07c83195210cbaef5a90bd1434a235b
Ajen Shireman <ajenshireman@gmail.com>
2014-04-30 15:21:02 -0400

    formatting

--- appstore/application/view/account/main.php
+++ appstore/application/view/account/main.php
@@ -3,8 +3,14 @@
         <h4>Account Details</h4>
     </div>
 	<div class="panelBody">
-	    <div>Username: <?php echo $this->user->get('username') ?></div>
-	    <div>Email: <?php echo $this->user->get('email' ) ?></div>
-	    <div>Joined: <?php echo $this->user->createDate() ?></div>
+	    <div>
+	       Username: <?php echo $this->user->get('username') ?>
+	   </div>
+	    <div>
+	       Email: <?php echo $this->user->get('email' ) ?>
+	    </div>
+	    <div>
+	       Joined: <?php echo $this->user->createDate() ?>
+	    </div>
 	</div>
 </div>
