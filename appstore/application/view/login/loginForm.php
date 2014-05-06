5915c477555c8edd2002c2a37dfde9f279b29767
Ajen Shireman <ajenshireman@gmail.com>
2014-05-06 12:32:04 -0400

    fixed check for existing email

--- appstore/application/view/login/loginForm.php
+++ appstore/application/view/login/loginForm.php
@@ -1,12 +1,22 @@
 <form name="loginForm" action="<?php echo URL ?>/login/login" method="post">
-    <?php if ( isset($this->FEEDBACK_NEGATIVE) ) { ?>
+    <?php 
+    if ( isset($this->FEEDBACK_NEGATIVE) ) { ?>
     <div>
         <?php 
-        foreach ( $this->FEEDBACK_NEGATIVE as $error ) {
-            echo '<div class="error">' . $error ."</div>\n";
+        foreach ( $this->FEEDBACK_NEGATIVE as $message ) {
+            echo '<div class="error">' . $message ."</div>\n";
         }
         ?>
     </div>
+    <?php 
+    } else if ( isset($this->FEEDBACK_POSITIVE) ) { ?>
+    <div>
+        <?php 
+        foreach ( $this->FEEDBACK_POSITIVE as $message ) {
+            echo '<div class="message_success">' . $message ."</div>\n";
+        }
+        ?>
+    </div> 
     <?php } ?>
     <div>
         <label for="loginUsername">Username: </label>
