69f7d9a6c76b1018cf968e52712494ed85b7bd67
Ajen Shireman <ajenshireman@gmail.com>
2014-05-07 13:14:54 -0400

    feedback printing moved to separate template

---
+++ appstore/application/view/_template/feedbackGeneral.php
@@ -0,0 +1,12 @@
+<?php if ( isset($this->FEEDBACK_GENERAL ) { ?>
+<div class="row">
+    <div class="small-12 column alert-box info" data-alert">
+        <?php 
+        foreach ( $this->FEEDBACK_GENERAL as $message ) {
+            echo '<p>' . $message."</p>\n";
+        }
+        ?>
+        <a href="#" class="close">&times;</a>
+    </div>
+</div>
+<?php } ?>
---
+++ appstore/application/view/_template/feedbackPositive.php
@@ -0,0 +1,12 @@
+<?php if ( isset($this->FEEDBACK_POSITIVE) ) { ?>
+<div class="row">
+    <div class="small-12 column alert-box success" data-alert">
+        <?php 
+        foreach ( $this->FEEDBACK_POSITIVE as $message ) {
+            echo '<p>' . $message."</p>\n";
+        }
+        ?>
+        <a href="#" class="close">&times;</a>
+    </div>
+</div>
+<?php } ?>
